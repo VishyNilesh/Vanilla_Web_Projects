0bcb094c6ab11b85e1d952b31eb0bfa9202f91cf
Vishy Nilesh <nileshkumar.v21@gmail.com>
2022-06-23 14:34:10 +0530

    demo.js

---
+++ demo.js
@@ -0,0 +1,5 @@
+var name=prompt("Enter your name")
+var age=prompt("Enter you age")
+agedays=age*365.25
+remainingdays=(60-age)*365.25
+alert("Hello "+name+"\n your current age is "+agedays+"days \n You will be their on earth for "+remainingdays+"days.No can change it until I want to")
