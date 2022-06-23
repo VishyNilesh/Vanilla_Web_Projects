e6de471aed06f810e60f66812477c68ad19fcdd2
Vishy Nilesh <nileshkumar.v21@gmail.com>
2022-06-23 14:34:45 +0530

    js example for testing whether number is even or not

---
+++ evenornot.js
@@ -0,0 +1,9 @@
+var num=prompt("Enter a value to check if it's even or not ?")
+if (num%2==0) {
+  console.log("given number is even")
+  alert("given number is even")
+}
+else {
+  console.log("Not an even number")
+  alert("Not an even number")
+}
