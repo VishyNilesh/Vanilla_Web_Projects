77309e016b1f4e81d8a52c35028f61ddc198b762
Vishy Nilesh <nileshkumar.v21@gmail.com>
2022-06-23 14:35:14 +0530

    js for age criteria matrimonial application website

---
+++ matrimonial.js
@@ -0,0 +1,10 @@
+var age=prompt("Enter your age")
+if (age<18) {
+  alert("Please wait sometime more, you will get marriages after 25")
+}
+else if (age>60) {
+  alert("arey budhey your age is gone for marriage now go & do bhajan kirtan")
+}
+else {
+  alert("thanks for registering you will get marriage soon by emails")
+}
