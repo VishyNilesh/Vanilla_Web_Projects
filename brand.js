8dec0f66585c872e5d922b284fda34b6022f5ee0
Vishy Nilesh <nileshkumar.v21@gmail.com>
2022-06-26 13:07:23 +0530

    js based application for basic selection using loops

---
+++ brand.js
@@ -0,0 +1,16 @@
+var brand=prompt("Enter the name of brand : ")
+if (brand=="KF") {
+  alert("It's a children brand")
+}
+else if (brand=="KO") {
+  alert("it's too light")
+}
+else if (brand=="RC") {
+  alert("it's not that much kick")
+}
+else if (brand=="FO") {
+  alert("Buy one get one free")
+}
+else {
+  alert("we dont serve this brand")
+}
