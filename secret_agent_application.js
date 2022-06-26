b2ef023d4bbb09aea1e765c610d0dd4d3aef57aa
Vishy Nilesh <nileshkumar.v21@gmail.com>
2022-06-26 13:08:55 +0530

    js based application for secret agent app

---
+++ secret_agent_application.js
@@ -0,0 +1,11 @@
+var name = prompt("Enter your name")
+var actor = prompt("Enter your fav actor name")
+var num  = prompt("Enter your lucky number")
+var dish = prompt("Enter the name of dish")
+if ((name[0]=="d")&&(actor[actor.length-1]=="r")&&(num==7)&&(dish.length>=6))
+{
+  console.log(name+ " " + "Next mission is to kill procastination & be successfull!!");
+}
+else {
+  console.log("thanks");
+}
